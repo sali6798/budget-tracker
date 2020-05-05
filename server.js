5b14421e27c2baab20bb60272d64d311134f824a
sali6798 <shaidee@alingcastre.net>
2020-05-04 22:53:11 -0700

    wip

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds149593.mlab.com:49593/heroku_s2mw37g9";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds149593.mlab.com:49593/heroku_s2mw37g9" || "mongodb://localhost/budget";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
